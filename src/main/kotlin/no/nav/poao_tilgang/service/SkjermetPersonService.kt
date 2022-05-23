38ad3b9f354262796fbaba6d8e3ce4a194df70ae
Alexander Gård <alex.gaard@hotmail.com>
2022-05-23 08:45:43 +0200

    La til caching av skjermet person

--- src/main/kotlin/no/nav/poao_tilgang/service/SkjermetPersonService.kt
+++ src/main/kotlin/no/nav/poao_tilgang/service/SkjermetPersonService.kt
@@ -1,20 +1,51 @@
 package no.nav.poao_tilgang.service
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.poao_tilgang.client.skjermet_person.SkjermetPersonClient
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class SkjermetPersonService(
 	private val skjermetPersonClient: SkjermetPersonClient
 ) {
 
+	private val norkIdentToErSkjermetCache = Caffeine.newBuilder()
+		.expireAfterWrite(Duration.ofHours(1))
+		.build<String, Boolean>()
+
 	fun erSkjermetPerson(norskIdent: String): Boolean {
-		val skjerming = skjermetPersonClient.erSkjermet(listOf(norskIdent))
-		return skjerming.getOrDefault(norskIdent, false)
+		return erSkjermetWithCache(listOf(norskIdent))
+			.getOrDefault(norskIdent, false)
 	}
 
 	fun erSkjermetPerson(norskeIdenter: List<String>): Map<String, Boolean> {
-		return skjermetPersonClient.erSkjermet(norskeIdenter)
+		return erSkjermetWithCache(norskeIdenter)
+	}
+
+	private fun erSkjermetWithCache(norskeIdenter: List<String>): Map<String, Boolean> {
+		val cachetSkjerming = mutableMapOf<String, Boolean>()
+		val manglendeSkjerming: MutableList<String> = mutableListOf()
+
+		norskeIdenter.forEach {
+			val erSkjermet = norkIdentToErSkjermetCache.getIfPresent(it)
+
+			if (erSkjermet != null) {
+				cachetSkjerming[it] = erSkjermet
+			} else {
+				manglendeSkjerming.add(it)
+			}
+		}
+
+		val skjerming = skjermetPersonClient.erSkjermet(manglendeSkjerming)
+
+		skjerming.forEach {
+			norkIdentToErSkjermetCache.put(it.key, it.value)
+		}
+
+		cachetSkjerming.putAll(skjerming)
+
+		return cachetSkjerming
 	}
 
 }
