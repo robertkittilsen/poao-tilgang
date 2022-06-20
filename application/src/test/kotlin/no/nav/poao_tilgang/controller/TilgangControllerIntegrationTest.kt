939fce7eec34953301a02f2c3bce4d8ce0a239ab
Matias Vinjevoll <matiasvinjevoll@gmail.com>
2022-06-20 11:29:10 +0200

    Flere tester for tilgang til modia (#6)

--- application/src/test/kotlin/no/nav/poao_tilgang/controller/TilgangControllerIntegrationTest.kt
+++ application/src/test/kotlin/no/nav/poao_tilgang/controller/TilgangControllerIntegrationTest.kt
@@ -3,8 +3,10 @@ package no.nav.poao_tilgang.controller
 import io.kotest.matchers.shouldBe
 import no.nav.poao_tilgang.client.microsoft_graph.AdGruppe
 import no.nav.poao_tilgang.core.domain.AdGrupper
+import no.nav.poao_tilgang.core.domain.DecisionDenyReason
 import no.nav.poao_tilgang.test_util.IntegrationTest
 import no.nav.poao_tilgang.utils.RestUtils.toJsonRequestBody
+import okhttp3.Response
 import org.junit.jupiter.api.Test
 import java.util.*
 
@@ -12,89 +14,113 @@ class TilgangControllerIntegrationTest : IntegrationTest() {
 
 	@Test
 	fun `harTilgangTilModia - should return 401 when not authenticated`() {
-		val response = sendRequest(
-			method = "POST",
-			path = "/api/v1/tilgang/modia",
-			body = """{"navIdent": "Z1234"}""".toJsonRequestBody()
-		)
+		val response = sendTilgangTilModiaRequest("Z1234")
 
 		response.code shouldBe 401
 	}
 
 	@Test
 	fun `harTilgangTilModia - should return 403 when not machine-to-machine request`() {
-		val response = sendRequest(
-			method = "POST",
-			path = "/api/v1/tilgang/modia",
-			body = """{"navIdent": "Z1234"}""".toJsonRequestBody(),
-			headers = mapOf("Authorization" to "Bearer ${oAuthServer.issueAzureAdToken()}")
-		)
+		val response = sendTilgangTilModiaRequest("Z1234") { oAuthServer.issueAzureAdToken() }
 
 		response.code shouldBe 403
 	}
 
 	@Test
-	fun `harTilgangTilModia - should return 'deny' if not member of correct ad group`() {
+	fun `harTilgangTilModia - should return 'deny' if not member of any ad group`() {
 		val navIdent = "Z12371"
-		val navAnsattAzureId = UUID.randomUUID()
 
-		val adGroupId1 = UUID.randomUUID()
-		val adGroupId2 = UUID.randomUUID()
+		mockAdGrupperResponse(listOf())
 
-		mockMicrosoftGraphHttpClient.enqueueHentAzureIdForNavAnsattResponse(
-			navAnsattAzureId
-		)
+		val response = sendTilgangTilModiaRequest(navIdent) { oAuthServer.issueAzureAdM2MToken() }
 
-		mockMicrosoftGraphHttpClient.enqueueHentAdGrupperForNavAnsatt(
-			listOf(adGroupId1, adGroupId2)
-		)
-
-		mockMicrosoftGraphHttpClient.enqueueHentAdGrupperResponse(
-			listOf(AdGruppe(adGroupId1, "Gruppe1"), AdGruppe(adGroupId2, "Gruppe2"))
-		)
-
-		val response = sendRequest(
-			method = "POST",
-			path = "/api/v1/tilgang/modia",
-			body = """{"navIdent": "$navIdent"}""".toJsonRequestBody(),
-			headers = mapOf("Authorization" to "Bearer ${oAuthServer.issueAzureAdM2MToken()}")
-		)
-
-		val expectedJson = """
-			{"decision":{"message":"NAV ansatt mangler tilgang til en av AD gruppene [0000-ga-bd06_modiagenerelltilgang, 0000-ga-modia-oppfolging, 0000-ga-syfo-sensitiv]","reason":"MANGLER_TILGANG_TIL_AD_GRUPPE","type":"DENY"}}
-		""".trimIndent()
-
-		response.code shouldBe 200
-		response.body?.string() shouldBe expectedJson
+		expectDeny(
+			response= response,
+			message = "NAV ansatt mangler tilgang til en av AD gruppene [0000-ga-bd06_modiagenerelltilgang, 0000-ga-modia-oppfolging, 0000-ga-syfo-sensitiv]",
+			reason = DecisionDenyReason.MANGLER_TILGANG_TIL_AD_GRUPPE)
 	}
 
 	@Test
-	fun `harTilgangTilModia - should return 'permit' if member of correct ad group`() {
+	fun `harTilgangTilModia - should return 'deny' if not member of correct ad group`() {
 		val navIdent = "Z12371"
-		val navAnsattAzureId = UUID.randomUUID()
 
-		val adGroupId1 = UUID.randomUUID()
-		val adGroupId2 = UUID.randomUUID()
+		mockAdGrupperResponse(listOf("Gruppe1", "Gruppe2"))
 
-		mockMicrosoftGraphHttpClient.enqueueHentAzureIdForNavAnsattResponse(
-			navAnsattAzureId
-		)
+		val response = sendTilgangTilModiaRequest(navIdent) { oAuthServer.issueAzureAdM2MToken() }
 
-		mockMicrosoftGraphHttpClient.enqueueHentAdGrupperForNavAnsatt(
-			listOf(adGroupId1, adGroupId2)
-		)
+		expectDeny(
+			response= response,
+			message = "NAV ansatt mangler tilgang til en av AD gruppene [0000-ga-bd06_modiagenerelltilgang, 0000-ga-modia-oppfolging, 0000-ga-syfo-sensitiv]",
+			reason = DecisionDenyReason.MANGLER_TILGANG_TIL_AD_GRUPPE)
+	}
 
-		mockMicrosoftGraphHttpClient.enqueueHentAdGrupperResponse(
-			listOf(AdGruppe(adGroupId1, AdGrupper.MODIA_OPPFOLGING), AdGruppe(adGroupId2, "Gruppe2"))
-		)
+	@Test
+	fun `harTilgangTilModia - should return 'permit' if member of modiagenerelltilgang`() {
+		val navIdent = "Z12371"
 
-		val response = sendRequest(
+		mockAdGrupperResponse(listOf(AdGrupper.MODIA_GENERELL))
+
+		val response = sendTilgangTilModiaRequest(navIdent) { oAuthServer.issueAzureAdM2MToken() }
+
+		expectPermit(response)
+	}
+
+	@Test
+	fun `harTilgangTilModia - should return 'permit' if member of modia-oppfolging`() {
+		val navIdent = "Z12371"
+
+		mockAdGrupperResponse(listOf(AdGrupper.MODIA_OPPFOLGING))
+
+		val response = sendTilgangTilModiaRequest(navIdent) { oAuthServer.issueAzureAdM2MToken() }
+
+		expectPermit(response)
+	}
+
+	@Test
+	fun `harTilgangTilModia - should return 'permit' if member of syfo-sensitiv`() {
+		val navIdent = "Z12371"
+
+		mockAdGrupperResponse(listOf(AdGrupper.SYFO_SENSITIV))
+
+		val response = sendTilgangTilModiaRequest(navIdent) { oAuthServer.issueAzureAdM2MToken() }
+
+		expectPermit(response)
+	}
+
+	@Test
+	fun `harTilgangTilModia - should return 'permit' if member of correct role and other`() {
+		val navIdent = "Z12371"
+
+		mockAdGrupperResponse(listOf(AdGrupper.MODIA_OPPFOLGING, "Gruppe2"))
+
+		val response = sendTilgangTilModiaRequest(navIdent) { oAuthServer.issueAzureAdM2MToken() }
+
+		expectPermit(response)
+	}
+
+	private fun mockAdGrupperResponse(adGrupperNavn: List<String>) {
+		val adGrupper = adGrupperNavn.map { AdGruppe(UUID.randomUUID(), it) }
+
+		mockMicrosoftGraphHttpClient.enqueueHentAzureIdForNavAnsattResponse(UUID.randomUUID())
+
+		mockMicrosoftGraphHttpClient.enqueueHentAdGrupperForNavAnsatt(adGrupper.map { it.id })
+
+		if (adGrupper.isNotEmpty()) {
+			mockMicrosoftGraphHttpClient.enqueueHentAdGrupperResponse(adGrupper)
+		}
+	}
+
+	private fun sendTilgangTilModiaRequest(navIdent: String, token: (() -> String)? = null): Response {
+		val headers = if (token != null) mapOf("Authorization" to "Bearer ${token()}") else mapOf()
+		return sendRequest(
 			method = "POST",
 			path = "/api/v1/tilgang/modia",
 			body = """{"navIdent": "$navIdent"}""".toJsonRequestBody(),
-			headers = mapOf("Authorization" to "Bearer ${oAuthServer.issueAzureAdM2MToken()}")
+			headers = headers
 		)
+	}
 
+	private fun expectPermit(response: Response) {
 		val expectedJson = """
 			{"decision":{"type":"PERMIT"}}
 		""".trimIndent()
@@ -103,4 +129,12 @@ class TilgangControllerIntegrationTest : IntegrationTest() {
 		response.body?.string() shouldBe expectedJson
 	}
 
+	private fun expectDeny(response: Response, message: String, reason: DecisionDenyReason) {
+		val expectedJson = """
+			{"decision":{"message":"$message","reason":"${reason.reason}","type":"DENY"}}
+		""".trimIndent()
+
+		response.code shouldBe 200
+		response.body?.string() shouldBe expectedJson
+	}
 }
