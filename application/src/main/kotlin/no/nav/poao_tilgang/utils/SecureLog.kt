771f2482c3c5d6c7c111bb794f332f69d1800bd2
Matias Vinjevoll <matias.vinjevoll@nav.no>
2022-07-06 14:15:59 +0200

    Fiks navn på SecureLog-logger (#18)

--- application/src/main/kotlin/no/nav/poao_tilgang/utils/SecureLog.kt
+++ application/src/main/kotlin/no/nav/poao_tilgang/utils/SecureLog.kt
@@ -3,5 +3,5 @@ package no.nav.poao_tilgang.utils
 import org.slf4j.LoggerFactory
 
 object SecureLog {
-	val secureLog = LoggerFactory.getLogger("secureLog")
+	val secureLog = LoggerFactory.getLogger("SecureLog")
 }
