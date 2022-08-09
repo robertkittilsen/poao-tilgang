bc343b5e32c855366d027fb6858df2fad9c81bc0
Matias Vinjevoll <matias.vinjevoll@nav.no>
2022-08-09 13:15:17 +0200

    Log resultat av tilgang til modia i secure logs (#11)
    * Log mer i resultat av tilgang til modia i secure logs
    
    * refaktorer navn

--- application/src/main/kotlin/no/nav/poao_tilgang/service/TilgangService.kt
+++ application/src/main/kotlin/no/nav/poao_tilgang/service/TilgangService.kt
@@ -1,9 +1,11 @@
 package no.nav.poao_tilgang.service
 
+import no.nav.common.log.MDCConstants
 import no.nav.poao_tilgang.core.domain.Decision
 import no.nav.poao_tilgang.core.domain.NavIdent
 import no.nav.poao_tilgang.core.policy.ModiaPolicy
 import no.nav.poao_tilgang.utils.SecureLog.secureLog
+import org.slf4j.MDC
 import org.springframework.stereotype.Service
 
 @Service
@@ -18,9 +20,29 @@ class TilgangService(
 	private fun <I> executePolicy(policyName: String, policyInput: I, policy: (input: I) -> Decision): Decision {
 		val decision = policy.invoke(policyInput)
 
-		secureLog.info("Policy result name=$policyName input=$policyInput decision=$decision")
+		secureLogPolicyResult(policyName, policyInput, decision)
 
 		return decision
+	}
+
+	private fun <I> secureLogPolicyResult(policyName: String, input: I, decision: Decision) {
+		val logLine = listOfNotNull(
+			logValueWithDescription(policyName, "Policy result name"),
+			logValueWithDescription(input, "input"),
+			logValueWithDescription(decision.type, "decision"),
+			logValueWithDescription(if (decision is Decision.Deny) decision.message else null, "denyMessage"),
+			logValueWithDescription(if (decision is Decision.Deny) decision.reason else null, "denyReason"),
+			logValueWithDescription(MDC.get(MDCConstants.MDC_CALL_ID), "callId"),
+			logValueWithDescription(MDC.get(MDCConstants.MDC_USER_ID), "userId"),
+			logValueWithDescription(MDC.get(MDCConstants.MDC_CONSUMER_ID), "consumerId"),
+			logValueWithDescription(MDC.get(MDCConstants.MDC_REQUEST_ID), "requestId")
+		).joinToString(" ")
+
+		secureLog.info(logLine)
+	}
+
+	private fun <T> logValueWithDescription(value: T?, description: String): String? {
+		return value?.let { "$description=$it" }
 	}
 
 }
