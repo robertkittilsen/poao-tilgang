31470d14e491dc4b6d6f9059989e6cc78d855694
Alexander Gård <alex.gaard@hotmail.com>
2022-09-16 19:17:40 +0200

    Fjernet config relatert til lokal kjøring
    Lokal kjøring vil ikke fungere så bra når ikke alle klienter er mocket ut. Anbefaler heller å lage tester som er gode nok til at man ikke trenger å kjøre opp lokalt.

--- application/src/test/kotlin/no/nav/poao_tilgang/application/LocalApplication.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.poao_tilgang.application
-
-import org.springframework.boot.SpringApplication
-
-fun main(args: Array<String>) {
-	val application = SpringApplication(Application::class.java)
-	application.setAdditionalProfiles("local")
-	application.run(*args)
-}
