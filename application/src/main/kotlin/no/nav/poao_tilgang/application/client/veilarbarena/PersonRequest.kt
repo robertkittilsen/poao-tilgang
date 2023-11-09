9b121278c9133a11753a239e1d77c488a1b70238
Oda Dahlen <oda.dahlen@nav.no>
2023-11-09 23:46:20 +0100

    Tatt i bruk nytt endepunkt

---
+++ application/src/main/kotlin/no/nav/poao_tilgang/application/client/veilarbarena/PersonRequest.kt
@@ -0,0 +1,7 @@
+package no.nav.poao_tilgang.application.client.veilarbarena
+
+import no.nav.common.types.identer.Fnr
+
+data class PersonRequest(
+	val fnr: Fnr
+)
--- application/src/main/kotlin/no/nav/poao_tilgang/application/client/veilarbarena/VeilarbarenaClient.kt
+++ application/src/main/kotlin/no/nav/poao_tilgang/application/client/veilarbarena/VeilarbarenaClient.kt
@@ -1,10 +1,9 @@
 package no.nav.poao_tilgang.application.client.veilarbarena
 
 import no.nav.poao_tilgang.core.domain.NavEnhetId
-import no.nav.poao_tilgang.core.domain.NorskIdent
 
 interface VeilarbarenaClient {
 
-	fun hentBrukerOppfolgingsenhetId(norskIdent: NorskIdent): NavEnhetId?
+	fun hentBrukerOppfolgingsenhetId(personRequest: PersonRequest): NavEnhetId?
 
 }
